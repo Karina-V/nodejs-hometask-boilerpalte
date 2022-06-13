16fc0945720c654286ffee47aeca8191629e4f07
Karina Vyzhovets <karinavizovec@gmail.com>
2022-06-14 01:41:45 +0300

    added methods to work with user

--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,34 @@ const { UserRepository } = require('../repositories/userRepository');
 class UserService {
 
     // TODO: Implement methods to work with user
+    getUsers() {
+        return UserRepository.getAll();
+    }
+
+    getUserById(id) {
+        return UserRepository.getOne({ id });
+    }
+
+    addUser(data) {
+        return UserRepository.create(data);
+    }
+
+    updateUser(id, data) {
+        return UserRepository.update(id, data);
+    }
+
+    deleteUser(id) {
+        return UserRepository.delete(id);
+    }
+
+    // create(userData) {
+    //     return UserRepository.create(userData) || null;
+    // }
+
+    // update(userId, userDataToUpdate) {
+    //     return UserRepository.update(userId, userDataToUpdate) || null;
+    // }
+
 
     search(search) {
         const item = UserRepository.getOne(search);
@@ -10,14 +38,6 @@ class UserService {
             return null;
         }
         return item;
-    }
-
-    create(userData) {
-        return UserRepository.create(userData) || null;
-    }
-
-    update(userId, userDataToUpdate) {
-        return UserRepository.update(userId, userDataToUpdate) || null;
     }
 }
 
